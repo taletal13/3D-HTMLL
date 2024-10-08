553f0f20ddf471278a908fb1eca5ece0b17a6acb
taletal13 <106343905+taletal13@users.noreply.github.com>
2024-10-08 11:34:44 -0300

    Add files via upload

--- informaçõeGlobais.js
+++ informaçõeGlobais.js
@@ -1,19 +1,35 @@
-const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json" console;log(url);
+const url = "https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json" 
+
+console;log(url);
 
 async function visualizarInformacoesGlobais() {
     const res = await fetch(url);
-    const dados = await res.json()
+    const dados = await res.json() 
+    const pessoasConectadas = (dados.total_pessoas_conectadas) / 1e9 
+    const pessoasNoMundo = (dados.total_pessoas_mundo) / 1e9
     const paragrafo = document.createElement("p")
-    paragrafo.classList.add("graficos-container__texto")
-    paragrafo.innerHTML = `Você sabia que o mundo tem
-    ${dados.total_pessoas_mundo} de pessoas e
-    que aproximadamente ${dados.total_pessoas_conectadas}
-    estão conectadas em alguma rede social e passam em média
-    ${dados.tempo_medio} horas conectadas.`
+    const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas)
+    * 100)
+    const porcentagemConectada = ((pessoasConectadas /
+    pessoasNoMundo) * 100).toFixed(2)
     const container = document.getElementById("graficos-container")
     container.appendChild(paragrafo)
-    console.log(paragrafo)
-    console.log(dados.tempo_medio);
+    
+   
+   
+   
+   
+    paragrafo.classList.add("graficos-container__texto")
+   
+    paragrafo.innerHTML = `Você sabia que o mundo tem
+    <span>${pessoasNoMundo} bilhões</span> de pessoas e
+    que aproximadamente <span>${pessoasConectadas} bilhões
+    </span> estão conectadas em alguma rede social e passam
+    em média <span>${horas}</span> horas e <span>${minutos}</span> minutos conectadas.`
+
+    <br> "Isso significa que aproximadamente ${porcentagemConectada}%de pessoas estão conectadas em alguma rede social."
+    
     }
 
     visualizarInformacoesGlobais()
@@ -23,11 +39,5 @@ async function visualizarInformacoesGlobais() {
         "tempo_medio"; 2.38; 
         "total_pessoas_mundo"; 7.888e9;
         }
-
-
-
-
-
-
 
 
