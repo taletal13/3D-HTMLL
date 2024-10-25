6e8fa0da5101f679c8eb958a2db518a7b78eb49c
taletal13 <106343905+taletal13@users.noreply.github.com>
2024-10-25 08:06:33 -0300

    Add files via upload

--- quantidadedaUsuarios.js
+++ quantidadedaUsuarios.js
@@ -38,8 +38,27 @@ import { getCSS } from "./common.js"
                 plot_bgcolor: getCSS("--bg-color"),
                 paper_bgcolor: getCSS("--bg-color"),
                 title: {
-                    text: 'Redes sociais com mais usuários no mundo'
+                    text: 'Redes sociais com mais usuários no mundo',
+                    x: 0,
+                    font: {
+                    color: getCSS('--primary-color'),
+                    family: getCSS('--font'),
+                   size: 30,
                     }
+                },xaxis: {title: {
+                    text: 'nome das redes sociais',
+                    font: {
+                    color: getCSS('--secundary-color')
+                    }
+                    }
+                }, yaxis: {title: {
+                    text: 'bilhões de usuários ativos',
+                    font: {
+                    color: getCSS('--secundary-color')
+                    }
+                    }
+                }
+
                 }
             
         }
